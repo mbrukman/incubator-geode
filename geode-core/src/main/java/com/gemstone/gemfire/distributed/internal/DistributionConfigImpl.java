8e8337ddeb05df1bb854b3dea70da26760dab7d4
Udo Kohlmeyer <ukohlmeyer@pivotal.io>
2016-06-21 13:37:04 +1000

    GEODE-420: Fixed ssl property settings for "javax.ssl.*"

--- geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/DistributionConfigImpl.java
+++ geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/DistributionConfigImpl.java
@@ -2071,6 +2071,14 @@ public class DistributionConfigImpl extends AbstractDistributionConfig implement
         // use sslProperties as base and let props with suffix GATEWAY_SSL_PROPS_SUFFIX override that base
         this.gatewaySslProperties.setProperty(attName, attValue);
       }
+      if (!this.httpServiceSSLProperties.containsKey(attName)) {
+        // use sslProperties as base and let props with suffix GATEWAY_SSL_PROPS_SUFFIX override that base
+        this.httpServiceSSLProperties.setProperty(attName, attValue);
+      }
+      if (!this.clusterSSLProperties.containsKey(attName)) {
+        // use sslProperties as base and let props with suffix GATEWAY_SSL_PROPS_SUFFIX override that base
+        this.clusterSSLProperties.setProperty(attName, attValue);
+      }
     }
   }
 
